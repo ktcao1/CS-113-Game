55aceabb2e45868d3d760187746bbd79ef853357
tristanb <54753791+tbtristan@users.noreply.github.com>
2021-11-13 16:41:26 -0800

    added dash mechanic

--- CS 113 Game/Assets/Scripts/PlayerMovement.cs
+++ CS 113 Game/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 movement;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private LayerMask dashLayerMask;
+    private bool isDashButtonDown = false;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isDashButtonDown = true;
+        }
     }
 
     private void FixedUpdate()
@@ -20,5 +26,18 @@ public class PlayerMovement : MonoBehaviour
         if (movement.x > 0) playerTransform.localScale = new Vector2(1, 1);
         else if (movement.x < 0) playerTransform.localScale = new Vector2(-1, 1);
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        if (isDashButtonDown)
+        {
+            float dashAmount = 0.65f;
+            Vector3 dashPosition = rb.position + movement * dashAmount;
+
+            RaycastHit2D raycastHit2d = Physics2D.Raycast(rb.position, movement, dashAmount, dashLayerMask);
+            if (raycastHit2d.collider != null) {
+                dashPosition = raycastHit2d.point;
+            }
+            rb.MovePosition(dashPosition);
+            isDashButtonDown = false;
+        }
     }
 }
